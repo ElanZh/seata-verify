b132f189edef7f62a3b428b63613b41cb3569bd7
张一然 <zhangyr@sunyard.com>
2019-07-01 15:03:57 +0800

    seataVerify-User
    删除target

---
+++ seataVerify-SeataServer/src/main/java/elan/verify/seata/user/App.java
@@ -0,0 +1,19 @@
+package elan.verify.seata.user;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class App {
+    private final static Logger LOG = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+        LOG.info("user-启动成功");
+    }
+
+}
